241e1f130ed3a18ead73c14e1730ddc762f2a95f
Projckt <projckt@protonmail.com>
2020-01-25 10:15:00 +0530

    connected to mongoDb

--- user_auth_sys_1/backend/index.js
+++ user_auth_sys_1/backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
+mongoose
+  .connect("mongodb://localhost:27017/local", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB connected..!"))
+  .catch(err => console.log(err));
 app.listen(PORT, console.log(`Server started on PORT: ${PORT}`));
