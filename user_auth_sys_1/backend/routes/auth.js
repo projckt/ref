88c069b49a73fb28ca92a3ce03d6d818ddf710f3
Projckt <projckt@protonmail.com>
2020-01-26 16:28:55 +0530

    adds Joi validation to /register inputs

--- user_auth_sys_1/backend/routes/auth.js
+++ user_auth_sys_1/backend/routes/auth.js
@@ -1,21 +1,38 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Joi = require("@hapi/joi");
 
-router.post("/register", async (req, res) => {
-  const user = new User({
-    fname: req.body.fname,
-    lname: req.body.lname,
-    email: req.body.email,
-    pass: req.body.pass
-  });
+const registerIpSchema = Joi.object({
+  fname: Joi.string()
+    .min(1)
+    .required(),
+  lname: Joi.string()
+    .min(1)
+    .required(),
+  email: Joi.string()
+    .min(5)
+    .required()
+    .email(),
+  pass: Joi.string()
+    .min(6)
+    .required()
+});
 
-  try {
-    const savedUser = await user.save();
-    res.send(savedUser);
-  } catch (err) {
-    res.status(400).end(err);
-  }
-  res.send("hit");
+router.post("/register", (req, res) => {
+  const validation = registerIpSchema.validate(req.body);
+  res.send(validation);
+  // const user = new User({
+  //   fname: req.body.fname,
+  //   lname: req.body.lname,
+  //   email: req.body.email,
+  //   pass: req.body.pass
+  // });
+  // try {
+  //   const savedUser = await user.save();
+  //   res.send(savedUser);
+  // } catch (err) {
+  //   res.status(400).end(err);
+  // }
 });
 
 module.exports = router;
