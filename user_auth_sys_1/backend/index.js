f5e71e8901f3698edf8aaa5436b699f0fa87e1f7
Projckt <projckt@protonmail.com>
2020-01-26 12:32:17 +0530

    connects to mongo

--- user_auth_sys_1/backend/index.js
+++ user_auth_sys_1/backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const auth = require("./routes/auth");
+const mongoose = require("mongoose");
 const PORT = 5000;
+
+mongoose.connect(
+  "mongodb://localhost/test",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log(`connected to database`);
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("lel");
