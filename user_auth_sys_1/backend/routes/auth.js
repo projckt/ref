95b4cadd5a5286770aa5d9211a233f4c85bef760
Projckt <projckt@protonmail.com>
2020-01-26 12:50:52 +0530

    tests /register with postman

--- user_auth_sys_1/backend/routes/auth.js
+++ user_auth_sys_1/backend/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 router.post("/register", (req, res) => {
-  res.send("register");
+  console.log(req.body);
+  res.send("hit");
 });
 module.exports = router;
