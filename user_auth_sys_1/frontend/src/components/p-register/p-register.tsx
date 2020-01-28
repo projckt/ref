98f79e457b3cc46cc51f73aa123b1a38e1340a3b
Projckt <projckt@protonmail.com>
2020-01-28 13:04:55 +0530

    prepares form

--- user_auth_sys_1/frontend/src/components/p-register/p-register.tsx
+++ user_auth_sys_1/frontend/src/components/p-register/p-register.tsx
@@ -5,18 +5,46 @@ import { Component, h } from "@stencil/core";
   styleUrl: "p-register.css"
 })
 export class PRegister {
+  handleFirstNameIp(event) {
+    console.log(`First Name: ${event.detail}`);
+  }
+  handleLastNameIp(event) {
+    console.log(`Lasr Name: ${event.detail}`);
+  }
+  handleEmailIp(event) {
+    console.log(`Email: ${event.detail}`);
+  }
+  handlePasswordIp(event) {
+    console.log(`Password: ${event.detail}`);
+  }
   render() {
     return (
       <div>
         <h2>Register</h2>
-        <input type="email" placeholder="First Name"></input>
-        <input type="email" placeholder="Last Name"></input>
+        <input
+          type="email"
+          placeholder="First Name"
+          onInput={(event: UIEvent) => this.handleFirstNameIp(event)}
+        ></input>
+        <input
+          type="email"
+          placeholder="Last Name"
+          onInput={(event: UIEvent) => this.handleLastNameIp(event)}
+        ></input>
         <br />
         <br />
-        <input type="email" placeholder="Your Email"></input>
+        <input
+          type="email"
+          placeholder="Your Email"
+          onInput={(event: UIEvent) => this.handleEmailIp(event)}
+        ></input>
         <br />
         <br />
-        <input type="email" placeholder="Password"></input>
+        <input
+          type="email"
+          placeholder="Password"
+          onInput={(event: UIEvent) => this.handlePasswordIp(event)}
+        ></input>
         <br />
         <br />
         <button>Register</button>
