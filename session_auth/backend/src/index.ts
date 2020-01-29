34729971d0d2f227ce3c2b77ff09206646b27a9d
Projckt <projckt@protonmail.com>
2020-01-30 00:34:36 +0530

    implements env

--- session_auth/backend/src/index.ts
+++ session_auth/backend/src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
 const app = express();
 
+const { ENV, PORT, OLO, GELLO } = process.env;
+
 app.get("/", (req, res) => {
   let resp: object = {
     status: "success",
     message: "YoYo"
   };
   res.json(resp);
+  console.log(` ENV: ${ENV}`);
+  console.log(` PORT: ${PORT}`);
+  console.log(` OLO: ${OLO}`);
+  console.log(` GELLO: ${GELLO}`);
 });
 
-app.listen(3666, () => {
+app.listen(PORT, () => {
   console.log(`Server is listening`);
 });
