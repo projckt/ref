3c68442135307c0ae18380208d89a579813a3522
Projckt <projckt@protonmail.com>
2020-01-31 22:08:14 +0530

    adds input validation on /register

--- session_auth/backend/src/routes/register.ts
+++ session_auth/backend/src/routes/register.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { registerSchema } from "../validation";
 const router = Router();
-router.post("/register", (req, res) => {
-  res.send(req.body);
+router.post("/register", async (req, res) => {
+  await registerSchema.validateAsync(req.body, { abortEarly: false });
+  const { fname, lname, email, password, passwordConfirmation } = req.body;
+  res.json({
+    fname: fname,
+    lname: lname,
+    email: email,
+    isPasswordMatching: true
+  });
 });
 
 export default router;
