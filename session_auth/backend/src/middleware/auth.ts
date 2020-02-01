25620890ea757bfca04f65eae9a5331adca1f2ed
Projckt <projckt@protonmail.com>
2020-02-02 01:30:26 +0530

    implements hashed password

--- session_auth/backend/src/middleware/auth.ts
+++ session_auth/backend/src/middleware/auth.ts
@@ -5,4 +5,6 @@ export const guest = (req: Request, res: Response, next: NextFunction) => {
   if (isLoggedIn(req)) {
     return next(new Error("you are already logged in!"));
   }
+
+  next();
 };
--- session_auth/backend/src/routes/register.ts
+++ session_auth/backend/src/routes/register.ts
@@ -3,11 +3,13 @@ import { registerSchema } from "../validation";
 import { User } from "../models/User";
 import { logIn } from "../helpers";
 import { guest } from "../middleware";
+import * as argon from "argon2";
 const router = Router();
 
 router.post("/register", guest, async (req, res) => {
   await registerSchema.validateAsync(req.body, { abortEarly: false });
-  const { fname, lname, email, password, passwordConfirmation } = req.body;
+  const { fname, lname, email, passwordConfirmation } = req.body;
+  let password = req.body.password;
   let isUserRegistered = await User.exists({ email });
   if (isUserRegistered) {
     let resp = {
@@ -16,6 +18,7 @@ router.post("/register", guest, async (req, res) => {
     };
     res.json(resp);
   }
+  password = await argon.hash(password, { type: argon.argon2id });
   const user = await User.create({
     fname,
     lname,
