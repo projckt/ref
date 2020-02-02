7d3310e1ed2ea091d271b96c7022f1242f4de27f
Projckt <projckt@protonmail.com>
2020-02-02 10:45:29 +0530

    implements logout

--- session_auth/backend/src/middleware/auth.ts
+++ session_auth/backend/src/middleware/auth.ts
@@ -5,6 +5,12 @@ export const guest = (req: Request, res: Response, next: NextFunction) => {
   if (isLoggedIn(req)) {
     return next(new Error("you are already logged in!"));
   }
+  next();
+};
 
+export const user = (req: Request, res: Response, next: NextFunction) => {
+  if (isLoggedIn(req)) {
+    return next(new Error("you must be logged in!"));
+  }
   next();
 };
--- session_auth/backend/src/routes/logout.ts
+++ session_auth/backend/src/routes/logout.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { logOut } from "../helpers";
 const router = Router();
-router.post("/logout", (req, res) => {
-  res.send("HIT on /logout");
+router.post("/logout", async (req, res) => {
+  await logOut(req, res);
 });
 
 export default router;
