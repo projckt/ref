23302ad2dde6591050d8cd158c41d2bd3b3061ab
Projckt <projckt@protonmail.com>
2020-02-07 11:11:24 +0530

    adds private route

--- session_auth/frontend/src/components/app-root/app-root.tsx
+++ session_auth/frontend/src/components/app-root/app-root.tsx
@@ -13,11 +13,29 @@ export class AppRoot {
           <stencil-route url="/" component="p-login" exact={true} />
           <stencil-route url="/register" component="p-register" />
           <stencil-route url="/login" component="p-login" />
-          <stencil-route url="/dashboard" component="p-dashboard" />
-          <stencil-route url="/settings" component="p-settings" />
+          {/* <stencil-route url="/dashboard" component="p-dashboard" />
+          <stencil-route url="/settings" component="p-settings" /> */}
+          <PrivateRoute url="/dashboard" component="p-dashboard" />
           <stencil-route component="p-catchall" />
         </stencil-route-switch>
       </stencil-router>
     );
   }
 }
+
+const PrivateRoute = ({ component, ...props }: { [key: string]: any }) => {
+  const Component = component;
+  // const redirectUrl = props.failureRedirect || "/login";
+  return (
+    <stencil-route
+      {...props}
+      routeRender={() => {
+        // if (auth.isAuthenticated) {
+        //   return <Component {...props} {...props.componentProps}></Component>;
+        // }
+        return <stencil-router-redirect url="/login"></stencil-router-redirect>;
+        // return <Component {...props} {...props.componentProps}></Component>;
+      }}
+    />
+  );
+};
