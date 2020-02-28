f7e984290cfc85b19bffeb34671a5731c00a7daf
Projckt <projckt@protonmail.com>
2020-02-28 10:16:33 +0530

    inits c-pricingbox

--- components/c-pricingbox/stencil.config.ts
+++ components/c-pricingbox/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'particle-systems-pricing',
+  namespace: 'c-pricingbox',
   outputTargets: [
     {
       type: 'dist',
