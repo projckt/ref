b60f2cd4f2f3c4b5586f82743fbe1055df4c6f57
T <48113882+projckt@users.noreply.github.com>
2020-06-03 10:01:52 +0530

    removes the componentWillLoad

--- backend/session_auth/frontend/src/components/app-root/app-root.tsx
+++ backend/session_auth/frontend/src/components/app-root/app-root.tsx
@@ -7,9 +7,7 @@ import { isUserLogged } from "../../utils/";
   shadow: true
 })
 export class AppRoot {
-  componentWillLoad() {
-    isUserLogged();
-  }
+
   render() {
     return (
       <stencil-router>
